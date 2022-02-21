1d9d2dd35b97f1867bdeb28e0cedb89d4c6223ad
ARKID KALECI <kaleciarkid@gmail.com>
2022-02-21 02:03:06 +0100

    init

---
+++ src/constants.tsx
@@ -0,0 +1,42 @@
+export const keyboardLetters = [
+  ['Q', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'Ë'],
+  ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ç'],
+  ['Z', 'X', 'C', 'V', 'B', 'N', 'M'],
+]
+
+export const letters = [
+  'A',
+  'B',
+  'C',
+  'Ç',
+  'D',
+  'E',
+  'Ë',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'X',
+  'Y',
+  'Z',
+]
+
+export const status = {
+  green: 'green',
+  yellow: 'yellow',
+  gray: 'gray',
+  unguessed: 'unguessed',
+}
